510e2bc4b91a6a91e67b2a400bd21b763b2f1728
DiegoPerez <andres199927@hotmail.com>
2023-11-23 20:27:55 -0500

    cambioFuncionRestar

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ func main() {
 	v := sumar(3, 4)
 	fmt.Println(v)
 
-	r := restar(5, 10)
+	r := restarDiego(5, 10)
 	fmt.Println(r)
 }
 
@@ -15,6 +15,6 @@ func sumar(v int, c int) int {
 	return v + c
 }
 
-func restar(a int, b int) int {
+func restarDiego(a int, b int) int {
 	return a - b
 }
